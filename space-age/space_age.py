c10a65cfe1dc6f56dff318314b29ef53da56440f
Kelly Farlow <kelly.farlow@gmail.com>
2018-08-20 22:51:36 +1000

    Rename methods and refactor

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -1,34 +1,33 @@
-SECONDS_PER_EARTH_YEAR = 31557600
-
-
 class SpaceAge(object):
     def __init__(self, seconds):
         self.seconds = seconds
 
+    def age_in_planet_years(self, orbital_period_in_earth_years):
+        SECONDS_PER_EARTH_YEAR = 31557600
+        seconds_per_planet_year = SECONDS_PER_EARTH_YEAR * orbital_period_in_earth_years
+        age_on_planet = self.seconds / seconds_per_planet_year
+        return round(age_on_planet, 2)
+
     def on_mercury(self):
-        return self.earth_years(0.2408467)
+        return self.age_in_planet_years(0.2408467)
 
     def on_venus(self):
-        return self.earth_years(0.61519726)
+        return self.age_in_planet_years(0.61519726)
 
     def on_earth(self):
-        return self.earth_years(1)
+        return self.age_in_planet_years(1)
 
     def on_mars(self):
-        return self.earth_years(1.8808158)
+        return self.age_in_planet_years(1.8808158)
 
     def on_jupiter(self):
-        return self.earth_years(11.862615)
+        return self.age_in_planet_years(11.862615)
 
     def on_saturn(self):
-        return self.earth_years(29.447498)
+        return self.age_in_planet_years(29.447498)
 
     def on_uranus(self):
-        return self.earth_years(84.016846)
+        return self.age_in_planet_years(84.016846)
 
     def on_neptune(self):
-        return self.earth_years(164.79132)
-
-    def earth_years(self, orbital_period):
-        age = self.seconds / (SECONDS_PER_EARTH_YEAR * orbital_period)
-        return round(age, 2)
+        return self.age_in_planet_years(164.79132)
